3216210136792b66b566fb6592fedc6a91664cb0
bane-rman <benmendoza2025@gmail.com>
2025-05-07 21:51:50 -0500

    Update webscraper.java
    Now only prints Primary weapons

--- webscraper.java
+++ webscraper.java
@@ -13,18 +13,31 @@ public class webscraper {
 
         try {
             final Document document = Jsoup.connect(url).get();
+            
+            final String titles = document.select("h3").text();
+            //System.out.println(titles);
+            final String[] titleList = titles.split("(?=\\p{Lu})");
+            // for (String s : titleList)
+            // {
+            //     System.out.println(s);
+            // }
+            int n = 0;
 
-            for (Element row : document.select("table.wikitable.grid tr")){
-                
-                final String ticker = row.select("a").text();
-                System.out.println(ticker);
-                final String ammo = row.select("td").text();
-                System.out.println(ammo);
+            for (Element table : document.select("table.wikitable.grid")){
+                if (n <= 5){
+                    System.out.println(titleList[n]);
+                    if (titleList[n].equals("Primary ")){
+                        for (Element row : table.select("table.wikitable.grid tr"))
+                        {
+                            //System.out.println(titleList[n]);
+                            System.out.println(row.select("a").text());
+                            
+                        }
+                    }
+                    
+                }
 
-                
-                
-                
-
+                n++;
             }
         } catch (Exception ex) {
             ex.printStackTrace();
